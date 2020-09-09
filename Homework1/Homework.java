a6c958382dbe9d0bf3a60054fbf028d2fc86f176
blueshelled99 <asc52@njit.edu>
2020-09-09 14:03:49 -0400

    added Homework1 folder and HW

---
+++ Homework1/Homework.java
@@ -0,0 +1,17 @@
+public class Homework {
+	public static void main(String[] args) {
+		// If it matches my sample exactly it gets a 0
+		// 1. Create a variable that holds a name
+		String fnameString = "Andy";
+		// 2. Output the name to the console
+		System.out.println("fname: " + fnameString);
+		// 3. Create a variable that holds an age (as a whole number)
+		int ageInteger = 22;
+		// 4. Output the age to the console
+		System.out.println("ageInteger: " + ageInteger);
+		// 5. Create a variable that holds a decimal
+		float decimalFloat = 99.99f;
+		// 6. Output the variable
+		System.out.println("decimalFloat: " + decimalFloat);
+	}
+}
