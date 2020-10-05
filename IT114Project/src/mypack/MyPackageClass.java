4edcb511dc1cf6c63b6d54bfd1c13b0591724625
blueshelled99 <asc52@njit.edu>
2020-10-05 14:01:41 -0400

    added lessons for OOP and socketspt3

---
+++ IT114Project/src/mypack/MyPackageClass.java
@@ -0,0 +1,8 @@
+package mypack;
+
+//from https://www.w3schools.com/java/java_packages.asp
+class MyPackageClass {
+	public static void main(String[] args) {
+		System.out.println("This is my package!");
+	}
+}
